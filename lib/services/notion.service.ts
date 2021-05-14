39f51c1876a0c0a788cef9fc517e0857e7984391
floross <floross@users.noreply.github.com>
2021-05-13 23:35:10 -0400

    feat: add a logger bridge from notion to a nestjs logger instance

--- lib/services/notion.service.ts
+++ lib/services/notion.service.ts
@@ -1,18 +1,30 @@
-import { Inject, Injectable } from '@nestjs/common';
-import { NotionModuleOptions } from '../interfaces';
-import { Client as NotionClient } from '@notionhq/client';
+import { Inject, Injectable, Logger as NestjsLogger } from '@nestjs/common';
+import {
+  Client as NotionClient,
+  Logger as NotionLogger,
+  LogLevel,
+} from '@notionhq/client';
 
+import { NotionModuleOptions } from '../interfaces';
 import { NOTION_MODULE_OPTIONS } from '../notion.constants';
 
 @Injectable()
 export class NotionService {
+  private readonly logger = new NestjsLogger(NotionService.name);
+
   protected _notion: NotionClient;
 
   constructor(
     @Inject(NOTION_MODULE_OPTIONS)
     protected readonly notionModuleOptions: NotionModuleOptions,
   ) {
-    this._notion = new NotionClient(notionModuleOptions);
+    const { nestjsLogger, ...notionOptions } = notionModuleOptions;
+
+    const logger = nestjsLogger ?? this.logger;
+    this._notion = new NotionClient({
+      logger: this.createLoggerBridge(logger),
+      ...notionOptions,
+    });
   }
 
   public get notion() {
@@ -41,5 +53,16 @@ export class NotionService {
 
   public get users() {
     return this.notion.users;
+  }
+
+  protected createLoggerBridge(nestjsLogger: NestjsLogger): NotionLogger {
+    return (
+      level: LogLevel,
+      message: string,
+      extraInfo: Record<string, unknown>,
+    ) => {
+      nestjsLogger[level](message);
+      nestjsLogger.verbose(extraInfo);
+    };
   }
 }
